d404337dc1d0fe59563471172f3ab194eb55dd36
Redth <jondick@gmail.com>
2013-11-30 21:16:44 -0500

    Shows how to make and use a custom theme

--- Samples/Android/Sample/MainActivity.cs
+++ Samples/Android/Sample/MainActivity.cs
@@ -9,7 +9,7 @@ using FlatUI;
 
 namespace Sample
 {
-	[Activity (Label = "FlatUI Sample", MainLauncher = true)]
+	[Activity (Label = "FlatUI.Xamarin Sample", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
 		FlatEditText edittextFlat;
@@ -25,14 +25,17 @@ namespace Sample
 		{
 			base.OnCreate (bundle);
 
+			//Ask for action bar to show
 			Window.RequestFeature (WindowFeatures.ActionBar);
 
+			//Setup our action bar title and icon
 			ActionBar.Title = "FlatUI.Xamarin Sample";
 			ActionBar.SetIcon (Android.Resource.Drawable.IcMenuInfoDetails);
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
+			//Wire up our flatui views
 			edittextFlat = FindViewById<FlatEditText> (Resource.Id.edittextFlat);
 			edittextFlat2 = FindViewById<FlatEditText> (Resource.Id.edittextFlat2);
 			seekbar = FindViewById<FlatSeekBar> (Resource.Id.seekbar);
@@ -42,14 +45,30 @@ namespace Sample
 			radioUncheckedDisabled = FindViewById<FlatRadioButton> (Resource.Id.radioUncheckedDisabled);
 			toggleUncheckedEnabled = FindViewById<FlatToggleButton> (Resource.Id.toggleUncheckedEnabled);
 
-			ChangeTheme (FlatTheme.Sky ());
+
+			//Create a custom theme very easily!
+			var customJabbrTheme = new FlatUI.FlatTheme () {
+				DarkAccentColor = Android.Graphics.Color.ParseColor("#00103f"),
+				BackgroundColor = Android.Graphics.Color.ParseColor("#003259"),
+				LightAccentColor = Android.Graphics.Color.ParseColor("#005191"),
+				VeryLightAccentColor = Android.Graphics.Color.ParseColor("#719fc3")
+			};
+
+			//Register the them so it will show up when we ask FlatUI for all the available themes
+			FlatUI.FlatUI.RegisterCustomTheme ("jabbr", customJabbrTheme);
+
+			//Change the theme to our custom one by default
+			ChangeTheme (customJabbrTheme);
 		}
 
 		void ChangeTheme(FlatTheme theme)
 		{
+			//Set default them
 			FlatUI.FlatUI.DefaultTheme = theme;
+			//Change the action bar
 			FlatUI.FlatUI.SetActionBarTheme (this, FlatUI.FlatUI.DefaultTheme, false);
 
+			//Set the theme on all existing controls
 			edittextFlat.Theme = theme;
 			edittextFlat2.Theme = theme;
 			seekbar.Theme = theme;
@@ -62,6 +81,7 @@ namespace Sample
 
 		public override bool OnCreateOptionsMenu (IMenu menu)
 		{
+			//Change theme button for the action bar
 			var action_item = menu.Add (new Java.Lang.String ("Change Theme"));
 			action_item.SetShowAsAction (ShowAsAction.Always);
 			action_item.SetIcon (Android.Resource.Drawable.IcMenuGallery);
@@ -71,6 +91,7 @@ namespace Sample
 
 		public override bool OnOptionsItemSelected (IMenuItem item)
 		{
+			//Show a list of theme choices to pick from 
 			var adBuilder = new AlertDialog.Builder (this);
 			adBuilder.SetTitle ("Change Theme");
 
